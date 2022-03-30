dca792190e12fca220905e8ac88541ea3fc52b53
Miere Liniel Teixeira <miere00@gmail.com>
2022-03-30 13:01:11 +1100

    Removed copyright notice put by mistake

--- kos-core/tests/kos/core/VertxFuturesTest.java
+++ kos-core/tests/kos/core/VertxFuturesTest.java
@@ -1,20 +1,3 @@
-/*
- * Copyright © 2020 Secure Code Warrior BVBA. All rights reserved.
- * <p>
- * See your license agreement for the terms and conditions of use.
- * Any other usage without prior written consent of Secure Code Warrior BVBA is prohibited.
- * <p>
- * If there is no license agreement, this material may not be reproduced, displayed, printed, modified
- * or distributed in whole or in part in any manner or on any media without the prior written permission
- * of Secure Code Warrior BVBA. This material is provided 'as is' and 'as available'.
- * Secure Code Warrior BVBA expressly disclaims all warranties of any kind, whether express or implied,
- * including (without limitation) warranties of merchantability, fitness for a particular purpose and
- * non-infringement. Secure Code Warrior BVBA reserves the right to make changes or updates to this
- * material at any time without notice. In no event shall Secure Code Warrior BVBA be liable for any
- * damage (whether indirect, incidental, special, consequential, direct, loss of revenue or profit, loss
- * or corruption of data, loss of use, the cost of procuring replacement goods, opportunity loss, loss
- * of anticipated savings, resulting from or in connection with your use of this material.
- */
 package kos.core;
 
 import io.vertx.core.Future;
